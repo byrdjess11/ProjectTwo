003381ed8fc9fbf125cccaa97e90ead2852c60e8
Tristan <magombotristan@gmail.com>
2019-05-09 21:12:29 -0700

    Added some html to login page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,17 +1,6 @@
-var db = require("../models");
-
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     res.redirect("signup");
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
   });
 };
