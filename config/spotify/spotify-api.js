1569f668e6fcc16615d2b63d837ed6ac7369f117
mchlgrca <mgarcia0898@gmail.com>
2019-05-14 19:48:24 -0700

    accountLogin() added to spotify-api

--- config/spotify/spotify-api.js
+++ config/spotify/spotify-api.js
@@ -2,6 +2,20 @@ var express = require("express");
 
 var app = express();
 
-getPlaylists = function() {
-  app.get();
+accountLogin = function() {
+  app.get("/login", function(req, res) {
+    var scopes = "user-read-private user-read-email";
+    res.redirect(
+      "https://accounts.spotify.com/authorize" +
+        "?response_type=code" +
+        "&client_id=" +
+        // eslint-disable-next-line camelcase
+        my_client_id +
+        (scopes ? "&scope=" + encodeURIComponent(scopes) : "") +
+        "&redirect_uri=" +
+        encodeURIComponent(redirect_uri)
+    );
+  });
 };
+
+accountLogin();
